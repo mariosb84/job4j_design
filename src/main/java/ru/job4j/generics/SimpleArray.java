c133b846a70b6c0f5d28e7a1fefe1bf2d31ea9f7
Sergei Baranov <mariosb84@mail.ru>
2021-03-21 19:49:26 +0300

    5.2.1. Реализовать SimpleArray<T> [#478386]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -1,58 +1,79 @@
 package ru.job4j.generics;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
-public class SimpleArray {
-    private final int[] array;
+public class SimpleArray<T> implements Iterator {
+    private final T[] array;
+    private final List array2 = new List();
     private  int index = 0;
+    private  int position = 0;
 
-    public SimpleArray(int[] array) {
+    public SimpleArray(T[] array) {
         this.array = array;
     }
 
-    public void add(int model) {
-    array[Objects.checkIndex(index, array.length)] = model;
+    public void add(T model) {
+    array[Objects.checkIndex(index, array.length)] =  model;
     index++;
     }
-    public boolean set(int index, int model) {
+    public boolean set(int index, T model) {
         boolean result = false;
-       if (get(index) != -1) {
+       if (get(index) != null) {
            array[Objects.checkIndex(index, array.length)] = model;
            result = true;
        }
        return result;
     }
     public boolean remove(int index) {
-        //index = Objects.checkIndex(index, array.length);
         boolean result = false;
-        if (get(index) != -1) {
-            // System.arraycopy(items,index + 1, items, index, size - index);
-            //items[size - 1] = null;
-            // items.set(size - 1, null);
-            // size--;
-            System.arraycopy(array, index + 1, array, index, array.length - 1);
-           // array[array.length - 1] = Integer.parseInt(null);
+        if (get(index) != null) {
+            System.arraycopy(array, (Objects.checkIndex(index, array.length)) + 1,
+                    array, (Objects.checkIndex(index, array.length)), array.length - 1);
+            array[array.length - 1] = null;
             //this.index--;
             result = true;
         }
         return result;
     }
-    public int get(int index) {
-        int result = -1;
-           for (int obj : array) {
+    public  T get(int index) {
+        T result = null;
+       // List<T>arrayAsList = new Arrays.asList<T>(array);
+       // Iterator iterator = ((java.util.List) arrayAsList).iterator();
+        Iterator<T> iterator = array.iterator();                                                 // не работает?????
+        while (iterator.hasNext()) {
+            T obj = iterator.next();
+            if (obj.equals(array[Objects.checkIndex(index, array.length)])) {
+            result = obj;
+            break;
+            }
+        }
+              /* for (T obj : array) {
                obj = array[Objects.checkIndex(index, array.length)];
                    result = obj;
                    break;
-           }
+           }*/
 
         return  result;
     }
 
+    @Override
+    public boolean hasNext() {
+        return position < array.length;
+    }
+
+    @Override
+    public Object next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return position++;
+    }
     public static void main(String[] args) {
-        SimpleArray simpleArray = new SimpleArray(new int[3]);
+        SimpleArray simpleArray = new SimpleArray(new Object[3]);
         simpleArray.add(5);
         simpleArray.add(6);
         simpleArray.add(7);
@@ -71,6 +92,5 @@ public class SimpleArray {
         System.out.println(Arrays.toString(simpleArray.array));
 
     }
-
 
 }

d7f163e588d72fc176aad6d638e79fbc3d3e39ad
Sergei Baranov <mariosb84@mail.ru>
2021-05-06 22:46:56 +0300

    3. Переопределить только hashCode [#478395]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -8,14 +8,16 @@ public class User {
 
     private String name;
     private int children;
-    private Calendar birthday;
+    //private Calendar birthday;
+    private int birthday;
 
-    public User(String name, int children, Calendar birthday) {
+    //public User(String name, int children, Calendar birthday) {
+    public User(String name, int children, int birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
     }
-    @Override
+   /* @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
@@ -27,7 +29,7 @@ public class User {
         return children == user.children
                 && birthday == user.birthday
                 && Objects.equals(name, user.name);
-    }
+    }*/
 
     @Override
     public int hashCode() {
@@ -35,18 +37,22 @@ public class User {
     }
 
     public static void main(String[] args) {
-        //Calendar date = new GregorianCalendar(1990, 1, 2);
-        //date.get(1);
-        User userOne = new User("User", 10, Calendar.getInstance());
-        User userTwo = new User("User", 10, Calendar.getInstance());
+        //User userOne = new User("User", 10, Calendar.getInstance());
+        //User userTwo = new User("User", 10, Calendar.getInstance());
+        User userOne = new User("User", 10, 5);
+        User userTwo = new User("User", 10, 5);
         HashMap<User, Object> map = new HashMap<>();
         map.put(userOne, new Object());
         map.put(userTwo, new Object());
-        System.out.println(userOne.hashCode());
-        System.out.println(userTwo.hashCode());
-        System.out.println(map.get(userOne));
-        System.out.println(map.get(userTwo));
-        System.out.println(map.size());
-        System.out.println(map.entrySet());
+        int x = (userOne.hashCode()) ^ (userOne.hashCode() >>> 16);
+        int y = (userTwo.hashCode()) ^ (userTwo.hashCode() >>> 16);
+        System.out.println(x);
+        System.out.println(y);
+        int i = (16 - 1) & x;
+        int i2 = (16 - 1) & y;
+        System.out.println("Индекс бакета userOne: " + i);
+        System.out.println("Индекс бакета userTwo: " + i2);
+        System.out.println("Размер таблицы : " + map.size());
+        System.out.println("Состав таблицы : " + map.entrySet());
     }
 }

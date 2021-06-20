6437766f5d4fe256ecf1080f2cb831703074be2b
Sergei Baranov <mariosb84@mail.ru>
2021-06-21 00:28:30 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Info.java
@@ -0,0 +1,19 @@
+package ru.job4j.question;
+
+import java.util.Objects;
+
+public class Info {
+
+    private int added;
+    private int changed;
+    private int deleted;
+
+    public Info(int added, int changed, int deleted) {
+        this.added = added;
+        this.changed = changed;
+        this.deleted = deleted;
+    }
+
+    /* Геттеры, сеттеры, equals() & hashCode() */
+
+}

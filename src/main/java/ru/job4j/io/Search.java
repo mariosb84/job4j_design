c3666d7712217e492a8c23c6bb8a46fd12ffd069
Sergei Baranov <mariosb84@mail.ru>
2021-07-29 22:09:58 +0300

    4.1. Сканирование файловой системы. [#106929]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,14 +10,15 @@ import java.util.function.Predicate;
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        Files.walkFileTree(start, new PrintFiles());
-        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
+        search(start, p -> p.toFile()
+                .getName()
+                .endsWith("js"))
+                .forEach(System.out::println);
     }
 
-    public static List<Path> search(Path root, Predicate<Path> condition) {
-        //SearchFiles searcher = new SearchFiles(condition);
-       // Files.walkFileTree(root, searcher);
-       // return searcher.getPaths();
-        return null;
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
     }
 }

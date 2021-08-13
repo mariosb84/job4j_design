f5674ae86e49bfe879cd4bb931a4bab9f8b4b260
Sergei Baranov <mariosb84@mail.ru>
2021-08-13 17:33:04 +0300

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/MyCSVReader.java
+++ src/main/java/ru/job4j/io/MyCSVReader.java
@@ -23,28 +23,36 @@ public class MyCSVReader {
     public static void main(String[] args) {
         MyCSVReader myCsvReader = new MyCSVReader("sourceCsvFileScanner.csv", "; ",
                 "targetCsvFileScanner.csv", "name, age");
-        myCsvReader.writeCsv("name; age; birthDate; education; children"
+        myCsvReader.writeCsv("name; age; birthDate; education; children;"
                 + System.lineSeparator()
-                + "Ivan; 30; 20.05.1991; higher; one"
+                + "Ivan; 30; 20.05.1991; higher; one;"
                 + System.lineSeparator()
-                + "Oleg; 40; 20.05.1981; average; two"
+                + "Oleg; 40; 20.05.1981; average; two;"
                 + System.lineSeparator()
-                + "Petr; 50; 20.05.1971; professional; three"
+                + "Petr; 50; 20.05.1971; professional; three;"
                 + System.lineSeparator()
                 + "Pavel; 100; 20.05.1921; no; five");
         try (PrintWriter out = new PrintWriter(new FileOutputStream(myCsvReader.out))) {
             var data = myCsvReader.readCsv();
             Scanner scanner = new Scanner(new ByteArrayInputStream(data.getBytes()))
             .useDelimiter(myCsvReader.delimiter);
+            String[] nextLine = new String[100];
+            int count = 0;
             while (scanner.hasNext()) {
-                out.write(scanner.next() + " " + System.lineSeparator());
+                if (scanner.next().endsWith(";"+"")) {
+                    out.write(System.lineSeparator());
+                }
+                //out.write(scanner.next() + " " + System.lineSeparator());
+                out.write(Arrays.toString(new String[]{nextLine[count] = scanner.next()})
+                        + " ");
+                count++;
                 //System.out.println(Arrays.toString(scanner.toString().getBytes(StandardCharsets.UTF_8)));
             }
             CSVReader reader = new CSVReader(new FileReader(myCsvReader.path), ',' , '"' , 1);
             //Read CSV line by line and use the string array as you want
-            String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                    System.out.println(Arrays.toString(nextLine));
+            String[] nextLine2;
+            while ((nextLine2 = reader.readNext()) != null) {
+                    System.out.println(Arrays.toString(nextLine2));
             }
         } catch (Exception e) {
             e.printStackTrace();

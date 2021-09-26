b2f9adf4d816494b1e3f7396bc6ccbf3e5034159
Sergei Baranov <mariosb84@mail.ru>
2021-09-26 21:14:15 +0300

    2. Поиск файлов по критерию [#783]

--- src/main/java/ru/job4j/io/filesearchbycriterion/FileSearchByCriterion.java
+++ src/main/java/ru/job4j/io/filesearchbycriterion/FileSearchByCriterion.java
@@ -1,85 +1,60 @@
 package ru.job4j.io.filesearchbycriterion;
 
 import ru.job4j.io.ArgsName;
-import ru.job4j.io.MyCSVReader;
-
 import java.io.*;
-import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
+import java.util.function.Predicate;
+
+import static ru.job4j.io.Search.search;
 
 public class FileSearchByCriterion {
-    private final String path;
-    private final String delimiter;
-    private final String out;
-    private final String filter;
+    private final String directory;
+    private final String fileName;
+    private final String searchType;
+    private final String fileWrite;
 
-    public FileSearchByCriterion(String path, String delimiter, String out, String filter) {
-        this.path = path;
-        this.delimiter = delimiter;
-        this.out = out;
-        this.filter = filter;
+    public FileSearchByCriterion(String directory, String fileName, String searchType, String fileWrite) {
+        this.directory = directory;
+        this.fileName = fileName;
+        this.searchType = searchType;
+        this.fileWrite = fileWrite;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 4) {
-            throw new IllegalArgumentException("Path is null, delimiter is null, out is null, filter is null."
-                    + " Usage java -jar target/myCSVReader.jar -path=file.csv -delimiter=; -out=stdout -filter=name,age.");
+            throw new IllegalArgumentException("Directory is null, fileName is null, searchType is null, fileWrite is null."
+                    + " Usage java -jar target/fileSearchByCriterion.jar -d=directory -n=fileName -t=searchType -o=fileWrite.");
         }
         ArgsName argsName = ArgsName.of(args);
-        MyCSVReader myCsvReader = new MyCSVReader(argsName.get("path"), argsName.get("delimiter"),
-                argsName.get("out"), argsName.get("filter"));
-        myCsvReader.getColumnsStrings();
+        FileSearchByCriterion fileSearchByCriterion = new FileSearchByCriterion(argsName.get("d"), argsName.get("n"),
+                argsName.get("t"), argsName.get("o"));
+        Path start = Paths.get(argsName.get("d"));
+        List<Path> result = search(start, fileSearchByCriterion.searchChoice(argsName.get("n")));
+        fileSearchByCriterion.writeToFile(result, argsName.get("o"));
     }
-    public void getColumnsStrings() {
-        StringBuilder builder = new StringBuilder();
-        ArrayList<Integer> numberColumn = new ArrayList<>();
-        String inputLine;
-        boolean writeEnable = false;
-        int i, l;
-        try {
-            Scanner scanIn = new Scanner(new BufferedReader(
-                    new FileReader(this.path)));
-            scanIn.useDelimiter(this.delimiter);
-            while (scanIn.hasNextLine()) {
-                inputLine = scanIn.nextLine();
-                String[] inArray = inputLine.split(this.delimiter);
-                String[] filterArr = this.filter.split(",");
-                for (i = 0; i < inArray.length; i++) {
-                    for (l = 0; l < filterArr.length; l++) {
-                        if (inArray[i].equals(filterArr[l])) {
-                            writeEnable = true;
-                            numberColumn.add(i);
-                        }
-                    }
-                }
-                if (writeEnable) {
-                    String s = getOut(this.delimiter, numberColumn, inArray);
-                    if (this.out.equals("stdout")) {
-                        System.out.println(s);                                                       // вывод в консоль
-                    }
-                    builder.append(s).append(System.lineSeparator());
-                }
-            }
-            if (!(this.out.equals("stdout"))) {
-                writeCsv(builder.toString(), this.out);                                               // запись в файл
-            }
-
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
-    }
-    private String getOut(String regex, List<Integer> list, String[] array) {
-        StringBuilder out = new StringBuilder();
-        for (int i : list) {
-            out.append(array[i]).append(regex);
-        }
-        return out.toString();
-    }
-    public void writeCsv(String s, String path) {
+    private void writeToFile(List<Path> target, String path) {                                  // запись в файл
         try (BufferedWriter out = new BufferedWriter(new FileWriter(path))) {
-            out.write(s);
+            for (Path p : target) {
+                out.write(String.valueOf(p));
+                out.write(System.lineSeparator());
+            }
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
+    }
+    private Predicate<Path> searchChoice(String s) {
+        Predicate<Path> result = null;
+       if (this.searchType.equals("name")) {
+        result = p -> p.toFile()
+                .getName()
+                .contains(s);
+       }
+        if (this.searchType.equals("mask")) {
+            result = p -> p.toFile()
+                    .getName()
+                    .endsWith(s.substring(1));
+        }
+        return result;
     }
 }

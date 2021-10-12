f11d4407843aa9f56ced52c2598f451de2baa407
Sergei Baranov <mariosb84@mail.ru>
2021-10-12 19:23:26 +0300

    1. Схема прав пользователи и роли. [#1093 #214392]

--- SQL/usersItems5/create.sql
+++ SQL/usersItems5/create.sql
@@ -16,10 +16,20 @@ create table role_rules(
 	role_id int references role(id),
     rules_id int references rules(id)
 );
+create table category(
+	id serial primary key,
+	name varchar(255)
+);
+create table state(
+	id serial primary key,
+	name varchar(255)
+);
 create table item(
 	id serial primary key,
 	name varchar(255),
-	users_id int references users(id)
+	users_id int references users(id),
+	category_id int references category(id),
+	state_id int references state(id)
 );
 create table comments(
 	id serial primary key,
@@ -30,12 +40,4 @@ create table attachs(
 	id serial primary key,
 	name varchar(255),
 	item_id int references item(id)
-);
-create table category(
-	id serial primary key,
-	name varchar(255)
-);
-create table state(
-	id serial primary key,
-	name varchar(255)
 );

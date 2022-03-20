d544dcb0d31aa6cf79c7252ddae6204a2e10dbf5
Sergei Baranov <mariosb84@mail.ru>
2022-03-20 19:34:32 +0300

    2. Что такое TDD? [#4918 #276718]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,18 +1,17 @@
 package ru.job4j.tdd;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
-
+import org.junit.Ignore;
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class CinemaTest {
 
-    /*@Test
+
+    @Test
     public void whenBuy() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
@@ -30,7 +29,8 @@ public class CinemaTest {
         assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
-    @Test
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
     public void whenHaveTicked() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
@@ -38,20 +38,20 @@ public class CinemaTest {
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
         Ticket ticket2 = cinema.buy(account, 1, 1, date);
-        assertNull(ticket2);
     }
 
-    @Test
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
     public void whenNotTickedCurrentDate() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertNull(ticket);
     }
 
-    @Test
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
     public void whenNotTickedCurrentPlace() {
         Account account = new AccountCinema();
         Account account2 = new AccountCinema();
@@ -60,7 +60,6 @@ public class CinemaTest {
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
         Ticket ticket2 = cinema.buy(account2, 1, 1, date);
-        assertNull(ticket2);
     }
 
     @Test
@@ -69,5 +68,6 @@ public class CinemaTest {
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> false);
         assertThat(sessions.size(), is(0));
-    }*/
+    }
+
 }

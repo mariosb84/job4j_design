d49efe28bca0022c1ea81e80d0b9c2f4a89b1acd
Sergei Baranov <mariosb84@mail.ru>
2022-03-20 12:20:20 +0300

    2. Что такое TDD? [#4918 #276718]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.tdd;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -11,21 +12,62 @@ import java.util.List;
 
 public class CinemaTest {
 
-    @Test
+    /*@Test
     public void whenBuy() {
-       /* Account account = new AccountCinema();
+        Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertThat(ticket, is(new Ticket3D()));*/
+        assertThat(ticket, is(new Ticket3D()));
     }
 
     @Test
     public void whenFind() {
-        /*Cinema cinema = new Cinema3D();
+        Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-        assertThat(sessions, is(Arrays.asList(new Session3D())));*/
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
+
+    @Test
+    public void whenHaveTicked() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertNull(ticket2);
+    }
+
+    @Test
+    public void whenNotTickedCurrentDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertNull(ticket);
+    }
+
+    @Test
+    public void whenNotTickedCurrentPlace() {
+        Account account = new AccountCinema();
+        Account account2 = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account2, 1, 1, date);
+        assertNull(ticket2);
+    }
+
+    @Test
+    public void whenNotSession() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        assertThat(sessions.size(), is(0));
+    }*/
 }

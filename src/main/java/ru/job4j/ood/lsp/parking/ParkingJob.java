f3e49cdda65541dde201175535e781beabe2b6c3
Sergei Baranov <mariosb84@mail.ru>
2022-04-15 22:54:30 +0300

    2. Парковка машин [#853]

--- src/main/java/ru/job4j/ood/lsp/parking/ParkingJob.java
+++ src/main/java/ru/job4j/ood/lsp/parking/ParkingJob.java
@@ -1,8 +1,7 @@
 package ru.job4j.ood.lsp.parking;
 
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.util.ArrayList;
+import java.util.List;
 
 public class ParkingJob implements Parking {
 
@@ -20,12 +19,12 @@ public class ParkingJob implements Parking {
 
     @Override
     public boolean accept(Auto auto) {
-      if (auto.getSpaceAmount() == 1 && truckSpace > 0) {
-          truckSpace--;
+      if (auto.getSpaceAmount() == 1 && carSpace > 0) {
+          carSpace--;
           carStorage.add(auto);
           return true;
-      } else if (auto.getSpaceAmount()  > 1 && carSpace > 0) {
-            carSpace--;
+      } else if (auto.getSpaceAmount()  > 1 && truckSpace > 0) {
+            truckSpace--;
             truckStorage.add(auto);
             return true;
         } else if (auto.getSpaceAmount() <= carSpace) {
@@ -37,18 +36,29 @@ public class ParkingJob implements Parking {
     }
 
     public static void main(String[] args) {
-        ParkingJob parkingJob = new ParkingJob(10, 50);
-        Auto car = new Car(1, "1");
-        Auto car2= new Car(1, "2");
-        Auto car3 = new Car(1, "3");
-        Auto car4 = new Car(1, "4");
-        Auto car5 = new Car(1, "5");
-        Auto truck = new Truck(2, "11");
-        Auto truck2 = new Truck(3, "22");
-        Auto truck3 = new Truck(4, "33");
-        Auto truck4 = new Truck(5, "44");
-        Auto truck5 = new Truck(10, "55");
-        System.out.println(parkingJob.accept(car));
+        ParkingJob parkingJob = new ParkingJob(1, 0);
+        List<Auto> list = List.of(
+                new Car(1, "1"),
+                new Car(1, "2"),
+                new Car(1, "3"),
+                new Car(1, "4"),
+                new Car(1, "5"),
+                new Truck(2, "11"),
+                new Truck(3, "22"),
+                new Truck(4, "33"),
+                new Truck(5, "44"),
+                new Truck(1, "55")
+        );
+        list.forEach(parkingJob::accept);
+        System.out.println("car:");
+        System.out.println(parkingJob.carSpace);
+        System.out.println("truck:");
+        System.out.println(parkingJob.truckSpace);
+        System.out.println("car:");
+        System.out.println(parkingJob.carStorage.toString());
+        System.out.println("truck:");
+        System.out.println(parkingJob.truckStorage.toString());
+
     }
 
 }
--- src/test/java/ru/job4j/ood/ParkingJobTest.java
+++ src/test/java/ru/job4j/ood/ParkingJobTest.java
@@ -1,4 +1,60 @@
 package ru.job4j.ood;
 
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.ood.lsp.parking.Auto;
+import ru.job4j.ood.lsp.parking.Car;
+import ru.job4j.ood.lsp.parking.ParkingJob;
+import ru.job4j.ood.lsp.parking.Truck;
+import java.util.List;
+
 public class ParkingJobTest {
+
+    List<Auto> list = List.of(
+            new Car(1, "1"),
+            new Car(1, "2"),
+            new Car(1, "3"),
+            new Car(1, "4"),
+            new Car(1, "5"),
+            new Truck(2, "11"),
+            new Truck(3, "22"),
+            new Truck(4, "33"),
+            new Truck(5, "44"),
+            new Truck(1, "55")
+    );
+
+    @Test
+    public void whenCarParkingOnTheCarPark() {
+        ParkingJob parkingJob = new ParkingJob(1, 0);
+        boolean rsl = parkingJob.accept(list.get(0));
+        Assert.assertTrue(rsl);
+    }
+
+    @Test
+    public void whenCarNotParkingOnTheCarPark() {
+        ParkingJob parkingJob = new ParkingJob(0, 1);
+        boolean rsl = parkingJob.accept(list.get(0));
+        Assert.assertFalse(rsl);
+    }
+
+    @Test
+    public void whenTruckParkingOnTheTruckPark() {
+        ParkingJob parkingJob = new ParkingJob(0, 1);
+        boolean rsl = parkingJob.accept(list.get(5));
+        Assert.assertTrue(rsl);
+    }
+
+    @Test
+    public void whenTruckNotParkingOnTheTruckPark() {
+        ParkingJob parkingJob = new ParkingJob(1, 0);
+        boolean rsl = parkingJob.accept(list.get(5));
+        Assert.assertFalse(rsl);
+    }
+
+    @Test
+    public void whenTruckParkingOnTheCarPark() {
+        ParkingJob parkingJob = new ParkingJob(5, 0);
+        boolean rsl = parkingJob.accept(list.get(8));
+        Assert.assertTrue(rsl);
+    }
 }

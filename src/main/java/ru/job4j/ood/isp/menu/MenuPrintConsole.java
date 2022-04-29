60bc10ac96a3e3c92da3e9ecb84bcea7e4e6768c
Sergei Baranov <mariosb84@mail.ru>
2022-04-29 15:24:12 +0300

    1. Создать меню. [#4748 #292629]

--- src/main/java/ru/job4j/ood/isp/menu/MenuPrintConsole.java
+++ src/main/java/ru/job4j/ood/isp/menu/MenuPrintConsole.java
@@ -2,27 +2,17 @@ package ru.job4j.ood.isp.menu;
 
 public class MenuPrintConsole implements MenuPrinter {
 
-    private final String indent = "";
-
     @Override
     public void print(Menu menu) {
+        for (Menu.MenuItemInfo m : menu) {
+            long count = m.getNumber().chars().
+                    filter(ch -> ch == '.').count();
+            StringBuilder indent = new StringBuilder();
+            for (int i = 1; i < count; i++) {
+                indent.append("____");
+            }
+            System.out.println(indent + m.getNumber() + m.getName());
+        }
+      }
 
-        menu.forEach(i -> System.out.println(
-                indent + i.getNumber() + i.getName()));
-
-
-    }
-
-    public static void main(String[] args) {
-        ActionDelegate actionDelegate = System.out::println;
-        Menu menu = new SimpleMenu();
-        menu.add(Menu.ROOT, "Сходить в магазин", actionDelegate);
-        menu.add(Menu.ROOT, "Покормить собаку", actionDelegate);
-        menu.add("Сходить в магазин", "Купить продукты", actionDelegate);
-        menu.add("Купить продукты", "Купить хлеб", actionDelegate);
-        menu.add("Купить продукты", "Купить молоко", actionDelegate);
-        MenuPrintConsole menuPrintConsole = new MenuPrintConsole();
-        menuPrintConsole.print(menu);
-
-    }
-}
+   }
--- src/main/java/ru/job4j/ood/isp/menu/TODOApp.java
+++ src/main/java/ru/job4j/ood/isp/menu/TODOApp.java
@@ -2,4 +2,16 @@ package ru.job4j.ood.isp.menu;
 
 public class TODOApp {
 
+
+    public static void main(String[] args) {
+        ActionDelegate actionDelegate = System.out::println;
+        Menu menu = new SimpleMenu();
+        menu.add(Menu.ROOT, "Сходить в магазин", actionDelegate);
+        menu.add(Menu.ROOT, "Покормить собаку", actionDelegate);
+        menu.add("Сходить в магазин", "Купить продукты", actionDelegate);
+        menu.add("Купить продукты", "Купить хлеб", actionDelegate);
+        menu.add("Купить продукты", "Купить молоко", actionDelegate);
+        MenuPrintConsole menuPrintConsole = new MenuPrintConsole();
+        menuPrintConsole.print(menu);
+    }
 }

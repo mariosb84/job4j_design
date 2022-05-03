48ede93f66200f885223dc0c7ff8b8ee0f9188ff
Sergei Baranov <mariosb84@mail.ru>
2022-05-03 21:08:00 +0300

    1. Создать меню. [#4748 #292629]

--- src/main/java/ru/job4j/ood/isp/menu/TODOApp.java
+++ src/main/java/ru/job4j/ood/isp/menu/TODOApp.java
@@ -1,16 +1,20 @@
 package ru.job4j.ood.isp.menu;
 
+import java.util.Scanner;
+
 public class TODOApp {
 
 
     public static void main(String[] args) {
         ActionDelegate actionDelegate = System.out::println;
         Menu menu = new SimpleMenu();
-        menu.add(Menu.ROOT, "Сходить в магазин", actionDelegate);
-        menu.add(Menu.ROOT, "Покормить собаку", actionDelegate);
-        menu.add("Сходить в магазин", "Купить продукты", actionDelegate);
-        menu.add("Купить продукты", "Купить хлеб", actionDelegate);
-        menu.add("Купить продукты", "Купить молоко", actionDelegate);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите корневой элемент");
+        menu.add(Menu.ROOT, scanner.nextLine(), actionDelegate);
+        System.out.println("Введите еще один корневой элемент");
+        menu.add(Menu.ROOT, scanner.nextLine(), actionDelegate);
+        System.out.println("Введите еще раз первый корневой элемент и его подменю");
+        menu.add(scanner.nextLine(), scanner.nextLine(), actionDelegate);
         MenuPrintConsole menuPrintConsole = new MenuPrintConsole();
         menuPrintConsole.print(menu);
     }
--- src/test/java/ru/job4j/ood/SimpleMenuTest.java
+++ src/test/java/ru/job4j/ood/SimpleMenuTest.java
@@ -1,12 +1,15 @@
 package ru.job4j.ood;
 
+import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.ood.isp.menu.ActionDelegate;
-import ru.job4j.ood.isp.menu.Menu;
-import ru.job4j.ood.isp.menu.SimpleMenu;
+import ru.job4j.ood.isp.menu.*;
+import ru.job4j.ood.lsp.food.Food;
+import ru.job4j.ood.lsp.food.Fruit;
 
 import java.util.List;
+import java.util.Optional;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class SimpleMenuTest {
@@ -40,6 +43,32 @@ public class SimpleMenuTest {
                 menu.select("Покормить собаку").get()
         );
         menu.forEach(i -> System.out.println(i.getNumber() + i.getName()));
+    }
+
+    @Test
+    public void whenSelectThenReturnSame() {
+        Menu menu = new SimpleMenu();
+        menu.add(Menu.ROOT, "Сходить в магазин", STUB_ACTION);
+        menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
+        menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
+        menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
+        Menu.MenuItemInfo expected = new Menu.MenuItemInfo(
+                "Сходить в магазин", List.of("Купить продукты"), STUB_ACTION, "1."
+        );
+        Menu.MenuItemInfo rsl = menu.select("Сходить в магазин").get();
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenPrintThenReturnSame() {
+        Menu menu = new SimpleMenu();
+        menu.add(Menu.ROOT, "Сходить в магазин", STUB_ACTION);
+        menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
+        menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
+        menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
+        MenuPrintConsole menuPrintConsole = new MenuPrintConsole();
+        menuPrintConsole.print(menu);
+
     }
 
 }
